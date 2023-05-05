988f2267a751e6d50fdae6a6f499e16f295f3300
Luís Gonçalves <84470810+Alektryon@users.noreply.github.com>
2023-05-05 16:24:58 +0100

    Update gematriaNGG.js

--- calc/gematriaNGG.js
+++ calc/gematriaNGG.js
@@ -48,12 +48,12 @@ class cipher {
 				this.vArr2 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]
 				}
 				break;
-//			case "AQsatanic":
-//				for (y = 0; y < 62; y++) {
-//				this.cArr = [48,49,50,51,52,53,54,55,56,57,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90]
-//				this.vArr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61]
-//				}
-//				break;
+			case "AQsatanic":
+				for (y = 0; y < 62; y++) {
+				this.cArr = [48,49,50,51,52,53,54,55,56,57,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90]
+				this.vArr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61]
+				}
+				break;
 			case "Latin":
 				for (y = 0; y < 26; y++) {
 				this.cArr = [97, 98, 99, 100, 101, 102, 103, 104, 105, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 120, 121, 122, 106, 118, 119]
@@ -189,7 +189,6 @@ class cipher {
 			}
 		}
 	}
-
 		if (GemTotal > 999999) {
 			return ">1 mil"
 		} else if (impType == 1) {
@@ -687,7 +686,7 @@ function Build_Ciphers() {
 			case "Keypad": allCiphers[allCiphers.length] = new cipher(key, "Keypad", 255, 126, 255); break;
 			case "Fibonacci": allCiphers[allCiphers.length] = new cipher(key, "Fibonacci", 233, 202, 148); break;
 			case "Alphanumeric Qabbala": allCiphers[allCiphers.length] = new cipher(key, "AQ", 190, 190, 126); break;
-//			case "Alphanumeric Satanic": allCiphers[allCiphers.length] = new cipher(key, "AQsatanic", 224,162,92); break;
+			case "Alphanumeric Satanic": allCiphers[allCiphers.length] = new cipher(key, "AQsatanic", 224,162,92); break;
 
 			case "Hebrew Reduction": allCiphers[allCiphers.length] = new cipher(key, "Hebrew G", 255, 189, 2, "FullReduction"); break;
 			case "Hebrew Ordinal": allCiphers[allCiphers.length] = new cipher(key, "Hebrew G", 255, 209, 36); break;
